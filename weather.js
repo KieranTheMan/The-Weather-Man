eb40397d4b6d919c16f6a79f0483c302e87a1b25
KieranTheMan <kieran9fowler@gmail.com>
2020-02-20 06:16:23 +0000

    add veribales for wDay, wMonth, iconValue

---
+++ weather.js
@@ -0,0 +1,18 @@
+const wDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const wMonth = ['January', 'February', 'March', 'April', 'May',
+    'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const iconValue = {
+        CLEARDAY: 'clear-day',
+        CLEARNIGHT: 'clear-night',
+        RAIN: 'rain',
+        SNOW: 'snow',
+        SLEET: 'sleet',
+        WIND: 'wind',
+        FOG: 'fog',
+        CLOUDY: 'cloudy',
+        PARTLY_CLOUDY_DAY: 'partly-cloudy-day',
+        PARTLY_CLOUDY_NIGHT: 'partly-cloudy-night'
+    }
+
+    
