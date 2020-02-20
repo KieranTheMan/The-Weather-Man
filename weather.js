6983c6ae2a74a916ffbaa82f0614dc711d0e4fe6
KieranTheMan <kieran9fowler@gmail.com>
2020-02-20 06:54:54 +0000

    create fetchWeatherReport function add DsProxyLink and DsApiLink include apikey, latitude-longitude

--- weather.js
+++ weather.js
@@ -15,4 +15,13 @@ const iconValue = {
         PARTLY_CLOUDY_NIGHT: 'partly-cloudy-night'
     }
 
+const apiKey = '415a6a77c9c19927d59b36e19cdd96fa';
+
+function fetchWeatherReport(apiKey, latitude, longitude) {
+
+    //to avoid cors issue
+    let DsProxyLink = 'https://cors-anywhere.herokuapp.com/';
+    let DsApiLink = `${DsProxyLink}https://api.darksky.net/`;
+
     
+}
